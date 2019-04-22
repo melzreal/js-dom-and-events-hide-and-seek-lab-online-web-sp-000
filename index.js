ad3a830042004a1b2d5aebee0d907b78bbd1b95c
Melissa Real <melissa.real@hotmail.com>
2019-04-22 09:50:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
- return document.querySelectorAll('#nested');
+ return document.querySelector('.target');
 }
+
+function increaseRankBy(n){
+  let goodies = document.querySelectorAll('ul.ranked-list li');
+  for (const element of goodies) {
+    goodies[element].innerHTML = parseInt(goodies[element].innerHTML) + n;
+    }
+}
+
+
+function deepestChild(){
+ return document.querySelector('div#grand-node div div div div');
+}
+
